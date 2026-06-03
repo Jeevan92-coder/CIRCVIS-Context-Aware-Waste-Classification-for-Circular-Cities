c004e4b2bafb6cc7e0c3cf08fc72e2b7988325e2
Meher Jeevan <jay95693@gmail.com>
2026-06-03 17:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(**name**)
 
 @app.route("/")
 def home():
-return "CIRCVIS Backend Running Successfully!"
+    return "CIRCVIS Backend Running Successfully!"
